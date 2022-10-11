c21b4046d1fef8d3473a8d3f80f5fa59b21188d5
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-11 20:44:56 +0900

    init

---
+++ turtle_insta/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for turtle_insta project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turtle_insta.settings')
+
+application = get_asgi_application()
---
+++ turtle_insta/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for turtle_insta project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turtle_insta.settings')
+
+application = get_wsgi_application()
