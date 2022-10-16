d78d776679fb63d12b9b6b780c5acc33afed17a6
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-16 16:27:18 +0900

    order_by create_at

--- community/views.py
+++ community/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render, redirect
 from community.models import Articles
 # Create your views here.
 def post_view(request):
-    all_article = Articles.objects.all()
-    print(all_article)
+    all_article = Articles.objects.all().order_by('-create_at')
+    context = {
+        'all_article':all_article
+    }
+
+    return render(request,'show.html',context)
+
     return HttpResponse('우선 유알엘 연결 선공?')
