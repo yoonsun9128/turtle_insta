abe74f38e5d5aa133983d242f42c29f8be213478
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-16 15:07:11 +0900

    articles model add

--- community/models.py
+++ community/models.py
@@ -1,3 +1,12 @@
+from turtle import title
+from xml.etree.ElementTree import Comment
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Articles(models.Model):
+    title = models.CharField(max_length=50)
+    commnet = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    edite_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
