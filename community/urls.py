a6766ecd3407c8b7861967847b2509f97d447d29
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-25 11:51:46 +0900

    게시물 디테일 페이지 형성

--- community/urls.py
+++ community/urls.py
@@ -6,4 +6,5 @@ app_name='community'
 urlpatterns = [
     path('',views.post_view, name='post_view'),
     path('write/',views.post_write, name='post_write'),
+    path('<int:article_id>/',views.article_detail, name="detail"),
 ]
--- community/views.py
+++ community/views.py
@@ -3,6 +3,7 @@ from turtle import title
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from community.models import Articles
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def post_view(request):
     all_article = Articles.objects.all().order_by('-create_at')
@@ -19,3 +20,10 @@ def post_write(request):
         content = request.POST.get('content')
         Articles.objects.save(title=title, content=content, user = request.user)
         return redirect('community:post_view')
+
+def article_detail(request,article_id):
+    article = get_object_or_404(Articles, id=article_id)
+    context = {
+        "article": article
+    }
+    return render(request,'detail.html',context)
